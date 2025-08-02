1238e74c5794b00ccea5a538463ac1aae417d289
bloodstiller <bloodstiller@bloodstiller.com>
2025-08-02 22:11:37 +0100

    removal of manual DNS in favour of tailscale, mullvad

--- modules/network.nix
+++ modules/network.nix
@@ -10,18 +10,26 @@
   };
 
   # DNS settings: prioritize local, then fallback
-  networking.nameservers =
-    [ "192.168.2.136" "192.168.2.195" "1.1.1.1" "8.8.8.8" ];
 
   # Enable TUN/TAP devices for VPN
   boot.kernelModules = [ "tun" ];
 
-  # Enable DNS resolution (required for VPN)
-  services.resolved = {
-    enable = true;
-    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
-    #dnssec = "false";  # optional; some local resolvers don’t support DNSSEC well
-  };
+  #DNS settings removed as tailscale with mullvad exit nodes being used. 
+  #networking.nameservers =
+  #[ "192.168.2.136" "192.168.2.195" "1.1.1.1" "8.8.8.8" ];
+
+  # Enable DNS resolution ( 
+  #services.resolved = {
+  #  enable = true;
+  #  fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
+  #  #dnssec = "false";  # optional; some local resolvers don’t support DNSSEC well
+  #};
+
+  #services.resolved.extraConfig = ''
+  #  DNS=192.168.2.136 192.168.2.195
+  #  FallbackDNS=1.1.1.1 8.8.8.8
+  #  Domains=~.
+  #'';
 
   # Enable the OpenSSH daemon
   services.openssh = {
@@ -31,17 +39,5 @@
       type = "ed25519";
     }];
   };
-
-  #networking.extraHosts = ''
-  # 192.168.2.113 radarr.homelab.bloodstiller.com sonarr.homelab.bloodstiller.com navidrome.homelab.bloodstiller.com nextcloud.homelab.bloodstiller.com
-  #192.168.2.12 truenas.homelab.bloodstiller.com
-
-  # '';
-
-  services.resolved.extraConfig = ''
-    DNS=192.168.2.136 192.168.2.195
-    FallbackDNS=1.1.1.1 8.8.8.8
-    Domains=~.
-  '';
 
 }
